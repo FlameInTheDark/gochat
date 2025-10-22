d31c19db72c9944936f024ab94d060141520505c
Viktor Menschikov <viktorfreedom@gmail.com>
2025-10-22 23:48:43 +0500

    Rebuild SFU room signaling around server offers

--- cmd/sfu/peer.go
+++ cmd/sfu/peer.go
@@ -5,89 +5,20 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/pion/webrtc/v3"
-
-	"github.com/FlameInTheDark/gochat/internal/mq/mqmsg"
+	"github.com/pion/webrtc/v4"
 )
 
 type peer struct {
-	userID     int64
-	pc         *webrtc.PeerConnection
-	send       func(op int, t int, d any) error
-	close      func()
-	log        *slog.Logger
+	userID int64
+	pc     *webrtc.PeerConnection
+	send   func(op int, t int, d any) error
+	close  func()
+	log    *slog.Logger
+
 	audioMuted atomic.Int32
-	mu         sync.Mutex
-	muted      map[int64]struct{}
 
-	// negotiation state
-	negoMu      sync.Mutex
-	negotiating bool
-	negoPending bool
-}
-
-// requestNegotiation coalesces renegotiation requests so only one offer is in-flight per peer.
-// If a negotiation is already running, marks it as pending to run another round after the answer.
-func (p *peer) requestNegotiation() {
-	p.negoMu.Lock()
-	if p.negotiating {
-		p.negoPending = true
-		p.negoMu.Unlock()
-		if p.log != nil {
-			p.log.Debug("nego: request queued", slog.Int64("user", p.userID))
-		}
-		return
-	}
-	p.negotiating = true
-	p.negoMu.Unlock()
-	if p.log != nil {
-		p.log.Debug("nego: start", slog.Int64("user", p.userID))
-	}
-	go p.doNegotiation()
-}
-
-func (p *peer) doNegotiation() {
-	offer, err := p.pc.CreateOffer(nil)
-	if err != nil {
-		p.negoMu.Lock()
-		p.negotiating = false
-		p.negoMu.Unlock()
-		if p.log != nil {
-			p.log.Warn("nego: create offer failed", slog.Int64("user", p.userID), slog.String("error", err.Error()))
-		}
-		return
-	}
-	if err := p.pc.SetLocalDescription(offer); err != nil {
-		p.negoMu.Lock()
-		p.negotiating = false
-		p.negoMu.Unlock()
-		if p.log != nil {
-			p.log.Warn("nego: set local failed", slog.Int64("user", p.userID), slog.String("error", err.Error()))
-		}
-		return
-	}
-	if p.log != nil {
-		p.log.Debug("nego: offer sent", slog.Int64("user", p.userID))
-	}
-	_ = p.send(int(mqmsg.OPCodeRTC), int(mqmsg.EventTypeRTCOffer), rtcOffer{SDP: offer.SDP})
-}
-
-// onAnswerProcessed should be called after remote answer is applied.
-// It starts a new negotiation if there were pending requests.
-func (p *peer) onAnswerProcessed() {
-	p.negoMu.Lock()
-	p.negotiating = false
-	if p.negoPending {
-		p.negoPending = false
-		p.negotiating = true
-		p.negoMu.Unlock()
-		if p.log != nil {
-			p.log.Debug("nego: follow-up", slog.Int64("user", p.userID))
-		}
-		go p.doNegotiation()
-		return
-	}
-	p.negoMu.Unlock()
+	mu    sync.Mutex
+	muted map[int64]struct{}
 }
 
 func (p *peer) SetSelfMuted(v bool) {
