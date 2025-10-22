083713925026e20eed00ad00a3d76f5aae36d807
Viktor Menschikov <viktorfreedom@gmail.com>
2025-10-22 23:09:08 +0500

    Resume pending SFU offers after signaling stabilizes

--- cmd/sfu/signaling_peer.go
+++ cmd/sfu/signaling_peer.go
@@ -44,8 +44,22 @@ func (a *App) setupPeer(room *room, uid int64, perms int64, send func(any) error
 	// Helpful: if the remote client can't handle server-initiated offers reliably,
 	// this will still be driven by explicit requestNegotiation() calls.
 	pc.OnNegotiationNeeded(func() {
+		// Avoid sending offers before the initial remote description is set to prevent glare.
+		if pc.CurrentRemoteDescription() == nil {
+			if a.log != nil {
+				a.log.Debug("nego: defer (no remote description)", slog.Int64("user", uid))
+			}
+			return
+		}
 		// Coalesce via requestNegotiation state machine
 		p.requestNegotiation()
+	})
+
+	pc.OnSignalingStateChange(func(state webrtc.SignalingState) {
+		if state != webrtc.SignalingStateStable {
+			return
+		}
+		p.resumePendingNegotiation("signaling stable")
 	})
 
 	pc.OnTrack(func(tr *webrtc.TrackRemote, _ *webrtc.RTPReceiver) {
